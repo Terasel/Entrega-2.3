836f776154b72eb4e6c0df85c894fc98f4ba55ff
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2024-06-25 12:34:57 +0200

    Adición función memoize

--- memoize.ts
+++ memoize.ts
@@ -1,1 +1,20 @@
-// NO se
+export const createCacheKeyFromArgs = (args: any[]) =>
+    args.reduce((cacheKey, arg) => (cacheKey += `_${typeof arg === 'object' ? JSON.stringify(args) : `${arg}`}_`), '');
+
+const memoize: (...args: any[]) => any = function(fn) {
+    const cache = new Map();
+  
+    return function(this:any) {
+      const cacheKey = createCacheKeyFromArgs(Array.from(arguments));
+  
+      if (cache.has(cacheKey)) {
+        return cache.get(cacheKey);
+      }
+  
+      const asyncFn = fn.apply(this, arguments);
+      cache.set(cacheKey, asyncFn);
+      return asyncFn;
+    };
+  };
+  
+  export default memoize;
